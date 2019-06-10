248eeee356b82a7c23093e4e863b7d27df0e803c
DropFL <18409052+DropFL@users.noreply.github.com>
2019-06-10 16:47:34 +0900

    `getattr.c` and `open.c` are added

---
+++ fuse_impl/getattr.c
@@ -0,0 +1,9 @@
+#include "../global.h"
+
+static int __getattr (const char *path, struct stat *st)
+{
+    //! TODO    IMPLEMENT IT FIRST!!!
+    return 0;
+}
+
+// getattr_type my_getattr = __getattr;
---
+++ fuse_impl/open.c
@@ -0,0 +1,9 @@
+#include "../global.h"
+
+static int __open (const char *path, struct fuse_file_info *info)
+{
+
+    return 0;
+}
+
+// open_type my_open = __open;
