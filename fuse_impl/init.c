d0975ca27e9e5642c91598470da5adac60d46b81
dnjsah1230@gmail.com <18409052+DropFL@users.noreply.github.com>
2019-06-13 03:26:57 +0900

    `my_init` copies the mount point's attributes

--- fuse_impl/init.c
+++ fuse_impl/init.c
@@ -2,20 +2,30 @@
 #include <string.h>
 #include <stdio.h>
 
-static Inode root_inode = {
-    .link_cnt = 1,
-};
-DirEntry root = {
-    .inode = &root_inode,
+static Inode root_inode =
+{
 };
 
-static void *__init (struct fuse_conn_info *info) {
-    // 현재 디렉토리 명을 root에 복사합니다.
+DirEntry root =
+{
+    .child  = NULL,
+    .parent = NULL,
+    .sibling= NULL,
+    .inode  = &root_inode,
+    .name   = "",
+};
 
-    char *cwd = (char *)fuse_get_context()->private_data;
-    strcpy(root.name, cwd);
+static void *__init (struct fuse_conn_info *info)
+{
+    struct stat *st = (struct stat*)fuse_get_context()->private_data;
 
-    printf("%s\n", cwd);
+    root_inode.uid = st->st_uid;
+    root_inode.gid = st->st_gid;
+    root_inode.mode = st->st_mode;
+    root_inode.link_cnt = st->st_nlink;
+    root_inode.a_time = st->st_atime;
+    root_inode.m_time = st->st_mtime;
+    root_inode.c_time = st->st_ctime;
 }
 
 init_type my_init = __init;
